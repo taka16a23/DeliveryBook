dff1a9cffc69c4fcaafd8d5b35432de937de512e
Atami <taka16a23@gmail.com>
2017-04-13 17:56:45 +0900

    Added Book list base activity.

---
+++ app/src/main/java/info/no_ip/taka16/deliverybook/book/BookListActivity.java
@@ -0,0 +1,15 @@
+package info.no_ip.taka16.deliverybook.book;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import info.no_ip.taka16.deliverybook.R;
+
+public class BookListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_list);
+    }
+}
