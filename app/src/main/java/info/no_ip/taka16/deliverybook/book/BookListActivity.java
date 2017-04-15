cf2bba3aaad65df8b93bf8522c3ace9e6e502261
Atami <taka16a23@gmail.com>
2017-04-16 07:02:04 +0900

    Removed unused packages.

--- app/src/main/java/info/no_ip/taka16/deliverybook/book/BookListActivity.java
+++ app/src/main/java/info/no_ip/taka16/deliverybook/book/BookListActivity.java
@@ -1,6 +1,5 @@
 package info.no_ip.taka16.deliverybook.book;
 
-import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -10,7 +9,6 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 
