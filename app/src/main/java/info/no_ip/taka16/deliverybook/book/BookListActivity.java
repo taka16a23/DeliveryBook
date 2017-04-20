e28a6e1b31242d7b0ff2ab2f4043af164ede6c39
Atami <taka16a23@gmail.com>
2017-04-20 16:15:03 +0900

    Remove unused package

--- app/src/main/java/info/no_ip/taka16/deliverybook/book/BookListActivity.java
+++ app/src/main/java/info/no_ip/taka16/deliverybook/book/BookListActivity.java
@@ -10,7 +10,6 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 
